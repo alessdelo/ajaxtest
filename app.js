d62a6b94ed6e03b1791db8bd2cf4e4e8f0775826
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-12-19 14:59:00 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+
+
+var app = express()
+  app.use('/static', express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: true}))
+
+
+ // app.use(bodyParser.json())
+ // app.use(bodyParser.urlencoded({extended: false}))
+ // app.use('/', home)
+ // app.use('/home', home)
+
+// temporary ajax test
+app.get('/ajaxTest', (req, res) => res.send("ajax1Test3 OK!"))
+
+// ---------------------------------
+// ---------------------------------
+// ---------------------------------
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
